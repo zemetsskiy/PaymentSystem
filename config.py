5ca3ddf4c414aab870ab0887baa64eaf7281d38d
realgeneral <artem.tretykov74@gmail.com>
2023-11-06 20:40:30 +0300

    empty config file

---
+++ config.py
@@ -0,0 +1,22 @@
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+REDIRECT_URI = ''
+SCOPE = ''
+REDIS_HOST = ""
+REDIS_PASSWD = ''
+REDIS_PORT = ""
+
+POLYGON_NODE_URL = "https://polygon-mainnet.infura.io/v3/7eec932e2c324e20ac051e0aa3741d9f"
+ARBITRUM_NODE_URL = "https://arbitrum-mainnet.infura.io/v3/7eec932e2c324e20ac051e0aa3741d9f"
+SEPOLIA_NODE_URL = "https://sepolia.infura.io/v3/7eec932e2c324e20ac051e0aa3741d9f"
+
+
+# ==== GMAIL =====
+GMAIL_USER = ''
+GMAIL_PASSWORD = ''
+
+# ==== DISCORD =====
+TOKEN = ""
+GUILD_ID = ""
+ROLE_NAME = ""
+MEMBER_ID = ""
